4cd7eddebec2b752d91987e880c2fb6abce789de
kawaicora <mengyou81219@163.com>
2026-05-08 12:38:57 +0800

    修复一些BUG 添加发布配置

---
+++ scripts/BuildRelease.bat
@@ -0,0 +1,13 @@
+for /f "usebackq delims=" %%i in (`
+    "%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" ^
+    -latest ^
+    -requires Microsoft.Component.MSBuild ^
+    -find MSBuild\**\Bin\MSBuild.exe
+`) do (
+    set "MSBUILD=%%i"
+)
+
+"%MSBUILD%" ..\Mochi.sln ^
+ /m ^
+ /p:Configuration=Release ^
+ /p:Platform=x86
